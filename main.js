5a3f3ae0204ce6b2987b56427c7921b1a2530140
Ken Garcia <kenrinfeld.garcia@gmail.com>
2017-08-19 13:52:59 +0800

    first Commit

---
+++ main.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+  var data = {
+  cars: [
+       {
+        "CarType": "Bmw",
+        "info":[{
+          "year" : "2017",
+          "model": [{
+            "mType" : "sedan",
+            "mType2" : "s4"
+          }]
+        }],
+        "carID": "bmw123"
+        },
+         {
+        "CarType": "Mercedes",
+        "info":[{
+          "year" : "2016",
+          "model": [{
+            "mType" : "Maybach",
+            "mType2" : "SLS"
+          }]
+        }],
+        "carID": "merc123"
+         },
+         {
+        "CarType": "Volvo",
+        "info":[{
+          "year" : "2015",
+          "model": [{
+            "mType" : "XC90",
+            "mType2" : "C30"
+          }]
+        }],
+        "carID": "vol123r"
+          },
+          {
+        "CarType": "Ford",
+        "info":[{
+          "year" : "2017",
+          "model": [{
+            "mType" : "Fiesta",
+            "mType2" : "Focus"
+          }]
+        }],
+        "carID": "ford123"
+          }
+     ]
+  };
+  var option, li, cType, info, year;
+  for(var x in data.cars){
+    cType   = data.cars[x].CarType;
+    info    = data.cars[x].info;
+    option  = $("<option class='cars' />").text(cType);
+    li      = $("<li class='cars' />").text(cType);
+
+    for(var y in data.cars[x].info){
+      $(".container").append(option);
+      $(".panel ul").append(li);
+        year = data.cars[x].info[y].year;
+        console.log(year);
+    }
+
+}
+
+
+
+});
